1d70f8f4338bb57f1c847c253f696e2fa4236c18
panesarravneet <panesarrishi8@gmail.com>
2023-08-24 16:47:03 +0930

    Updated User Interface (Output Interface)
    Worked on improving the user interface of the output to make it more presentable

--- Assessment - Software Unit Testing/guess_game.py
+++ Assessment - Software Unit Testing/guess_game.py
@@ -17,11 +17,12 @@ class NumGuessGame:
     def get_user_input(self):
         """Get a 4-digit number from the user or ask to quit."""
         while True:
-            user_input = input("Enter a 4-digit number or 'q' to quit: ")
+            user_input = input("\nEnter a 4-digit number or 'q' to quit: ")
             if user_input.lower() == 'q':
                 return None
+            # Checking for invalid input
             if not user_input.isdigit() or len(user_input) != 4:
-                print("Invalid input. Please enter a 4-digit number.")
+                print("\nInvalid input. Please enter a 4-digit number.")
             else:
                 return user_input
 
@@ -30,22 +31,22 @@ class NumGuessGame:
         hints = ''
         for i in range(4):
             if user_guess[i] == secret_number[i]:
-                hints += 'O'  # Correct digit in the correct position
+                hints += 'O'  # 'O' represents Correct digit in the correct position
             elif user_guess[i] in secret_number:
-                hints += 'X'  # Correct digit in the wrong position
+                hints += 'X'  # 'X' represents Correct digit in the wrong position
             else:
-                hints += '#'  # Incorrect digit
+                hints += '$'  # '$'represents Incorrect digit
         return hints
 
     def play_game(self):
         """Run Guess the Number game."""
-        print("Welcome to the Guess the Number game!")
+        print("     ---- Welcome To Guess The Number Game ----   ")
 
         while self.play_again:
             secret_number = self.generate_secret_number()
             attempts = 0
 
-            print("A random 4-digit number is generated. Try to guess it!")
+            print("(A random 4-digit number has been generated. Try to guess it.)")
 
             while True:
                 user_guess = self.get_user_input()
@@ -54,20 +55,23 @@ class NumGuessGame:
 
                 attempts += 1
                 hints = self.compare_numbers(secret_number, user_guess)
+                print("\t***************************")
                 print("\tAttempt:", attempts, "\tHints:", hints)
+                print("\t***************************")
 
                 # Check if the user has guessed the correct number.
                 if hints == 'OOOO':
-                    print("Congratulations! You guessed the number ", secret_number)
-                    print("Total number of attempts: ", attempts)
+                    print("\nCongratulations! You guessed the number", secret_number)
+                    print("\tTotal number of attempts: ", attempts)
                     break
 
             # Ask if the user wants to play again.
-            play_again = input("Do you want to play again? (yes/no): ").lower() == 'yes'
+            play_again = input("\nDo you want to play again? (yes/no): ").lower() == 'yes'
             self.play_again = play_again
 
-        print("Thanks for playing!")
+        print("\n     ----Thanks For Playing----     ")
 
 
-game = NumGuessGame()
-game.play_game()
+if __name__ == "__main__":
+    game = NumGuessGame()
+    game.play_game()
